e21bf2cfa6bd79cd16cf8cff5484b9a1c1fcd126
Scott Enriquez <sjenriquez@gmail.com>
2015-01-28 15:19:17 -0500

    Change index page to use show view partial.

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+  $('#like').click(event)
+    event.preventDefault();
 
+    $like = $(event.target);
+    $.ajax({
+      url: $like.attr('action'),
+      type: $like.attr('method'),
+      data: $like.serialize()
+    })
+    .done(function(response) {
+      $()
+    })
 
 });
