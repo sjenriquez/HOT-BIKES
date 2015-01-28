c210c72e89c3b809e3a58f7892360cf62c914adf
Scott Enriquez <sjenriquez@gmail.com>
2015-01-28 14:56:27 -0500

    Add small CSS changes for all views.

--- app/controllers/photo.rb
+++ app/controllers/photo.rb
@@ -8,14 +8,18 @@ get '/photos/new' do  # Route to new photo form
 end
 
 post '/photos' do     # Post route for new photo
-  erb :'photos/index'
+  p params
+  photo = Photo.create!(params[:photo])
+  current_user.photos << photo
+  redirect("/photos/#{photo.id}")
 end
 
 get '/photos/:id/edit' do
   erb :'photos/edit'
 end
 
-get '/photos/:id' do
+get '/photos/:id' do  # Display photo
+  @photo = Photo.find_by(id: params[:id])
   erb :'photos/show'
 end
 
@@ -25,4 +29,9 @@ end
 
 delete '/photos/:id' do
   erb :index
+end
+
+post '/like' do       # User likes a photo
+  current_user.liked_photos << Photo.find_by(id: params[:photo_id])
+  redirect("/photos/#{params[:photo_id]}")
 end
