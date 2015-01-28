be6737fc34a345382216907d3ecb60b66957212d
Scott Enriquez <sjenriquez@gmail.com>
2015-01-28 10:44:45 -0500

    Add auth routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,44 @@
 get '/' do
-  erb :welcome
+  if current_user
+    redirect '/photos'
+  else
+    erb :index
+  end
 end
 
 get '/login' do
-  
+  erb :'auth/login'
+end
+
+post '/login' do
+  user = User.find_by(email: params[:user][:email])
+
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/photos'
+  else
+    redirect '/login'
+  end
+
 end
 
 get '/signup' do
-  
+  erb :'auth/signup'
+end
+
+post '/signup' do
+  user = User.create(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect '/photos'
+  else
+    redirect '/signup'
+  end
+
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
