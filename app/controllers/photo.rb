d1a9c4e590de92587ea32ca8452228f4ee1acf3a
Scott Enriquez <sjenriquez@gmail.com>
2015-01-28 17:13:40 -0500

    Add routes to edit and delete photo, add edit view

--- app/controllers/photo.rb
+++ app/controllers/photo.rb
@@ -1,37 +1,40 @@
-get '/photos' do      # Display all photos from all users
+get '/photos' do            # Display all photos from all users
   @photos = Photo.all
   erb :'photos/index'
 end
 
-get '/photos/new' do  # Route to new photo form
+get '/photos/new' do        # Route to new photo form
   erb :'photos/new'
 end
 
-post '/photos' do     # Post route for new photo
-  p params
-  photo = Photo.create!(params[:photo])
+post '/photos' do           # Post route for new photo
+  photo = Photo.create(params[:photo])
   current_user.photos << photo
   redirect("/photos/#{photo.id}")
 end
 
-get '/photos/:id/edit' do
-  erb :'photos/edit'
+get '/photos/:id/edit' do   # Edit photo
+  photo = Photo.find_by(id: params[:id])
+  erb :'photos/edit', locals: {photo: photo}
 end
 
-get '/photos/:id' do  # Display photo
+get '/photos/:id' do        # Display photo
   photo = Photo.find_by(id: params[:id])
   erb :'photos/show', locals: {photo: photo}
 end
 
-put '/photos/:id' do
-  erb :'photos/show'
+put '/photos/:id' do        # Edit photo
+  photo = Photo.find_by(id: params[:id])
+  photo.update(title: params[:title])
+  redirect ("/photos/#{params[:id]}")
 end
 
-delete '/photos/:id' do
-  erb :index
+delete '/photos/:id' do   # Delete photo
+  Photo.find_by(id: params[:id]).destroy
+  redirect :'photos/index'
 end
 
-post '/like' do       # User likes a photo
+post '/like' do           # User likes a photo
   photo = Photo.find_by(id: params[:photo_id])
   current_user.liked_photos << photo
 
