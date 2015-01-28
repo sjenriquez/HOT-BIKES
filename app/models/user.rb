69785fcfe13b47bfe11aa42e9139387e8bfe4e36
Scott Enriquez <sjenriquez@gmail.com>
2015-01-28 10:12:33 -0500

    Add liked_photos association for User through Likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   has_many :photos
   has_many :likes
 
+  has_many :liked_photos, through: :likes, source: :photo
+
   validates :name, presence: true
   validates :email, :uniqueness => true, :format => /.+@.+\..+/
 
